a151012353b2fe5faad6847b6b4e2d9c3f3099e4
Filip31337 <filip.brajnovic@gmail.com>
2025-03-21 18:35:13 +0100

    add controllers

---
+++ race-application-command-service/src/main/java/org/brajnovic/controller/ApplicationController.java
@@ -0,0 +1,26 @@
+package org.brajnovic.controller;
+
+import org.brajnovic.entity.Application;
+import org.brajnovic.service.ApplicationService;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/command-service-api/applications")
+public class ApplicationController {
+    private final ApplicationService applicationService;
+
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping
+    public Application createApplication(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String club, @RequestParam UUID raceId) {
+        return applicationService.createApplication(firstName, lastName, club, raceId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteApplication(@PathVariable UUID id) {
+        applicationService.deleteApplication(id);
+    }
+}
---
+++ race-application-command-service/src/main/java/org/brajnovic/controller/RaceController.java
@@ -0,0 +1,34 @@
+package org.brajnovic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.brajnovic.entity.RaceDistance;
+import org.brajnovic.entity.Race;
+import org.brajnovic.service.RaceService;
+import org.springframework.web.bind.annotation.*;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/command-service-api/races")
+public class RaceController {
+    private final RaceService raceService;
+
+    public RaceController(RaceService raceService) {
+        this.raceService = raceService;
+    }
+
+    @PostMapping
+    public Race createRace(@RequestParam String name, @RequestParam RaceDistance distance) {
+        return raceService.createRace(name, distance);
+    }
+
+    @PutMapping("/{id}")
+    public Race updateRace(@PathVariable UUID id, @RequestParam String name, @RequestParam RaceDistance distance) {
+        return raceService.updateRace(id, name, distance);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteRace(@PathVariable UUID id) {
+        raceService.deleteRace(id);
+    }
+}
