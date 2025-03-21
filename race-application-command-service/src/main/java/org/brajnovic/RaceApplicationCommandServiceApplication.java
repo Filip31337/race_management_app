9cf3b8273dba24e706190c42c72a638f19b6bd6a
Filip31337 <filip.brajnovic@gmail.com>
2025-03-21 14:57:54 +0100

    add RaceApplicationCommandService using spring initializr

---
+++ race-application-command-service/src/main/java/org/brajnovic/RaceApplicationCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package org.brajnovic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceApplicationCommandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceApplicationCommandServiceApplication.class, args);
+	}
+
+}
